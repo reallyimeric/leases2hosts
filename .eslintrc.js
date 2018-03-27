105e5d43021c4e29c114af28aefacbde9e561756
Eric Little <reallyimeric@gmail.com>
2018-03-27 17:14:30 +0800

    added blabla

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "never"],
+        "no-console": ["warn", { allow: ["log", "warn", "error"] }],
+    },
+};
